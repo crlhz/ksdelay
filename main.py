e58c9368ab05acc405483e65afef40982d8c0c87
crlhz <horzela.kamil@gmail.com>
2024-10-29 12:32:20 +0100

    ref: main\

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ from scrapper import Scrapper
 from message import Message
 from bot import Bot
 
-
-scrapper = Scrapper("https://www.kolejeslaskie.com/category/informacje/")
-raw_messages = scrapper.get_data()
-
 old_messages = []
 messages = []
 unsent_messages = []
+
+scrapper = Scrapper("https://www.kolejeslaskie.com/category/informacje/")
+raw_messages = scrapper.get_data()
 
 load_dotenv()
 bot = Bot(os.getenv('BOT_TOKEN'))
 
 while True:
-    scrapper = Scrapper("https://www.kolejeslaskie.com/category/informacje/")
     raw_messages = scrapper.get_data()
     for mes in raw_messages:
         message = Message(mes)
