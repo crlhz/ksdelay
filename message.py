9673409c54c91492446dcc9515c2e7c360858c62
crlhz <horzela.kamil@gmail.com>
2024-10-29 11:55:34 +0100

    ref: add message doc

--- message.py
+++ message.py
@@ -2,9 +2,18 @@ import re
 
 
 class Message:
+    """
+    Representation of a single message
+
+    """
     def __init__(self, data):
+        """
+        Message constructor
+
+        @param data:     content of single message provided via parser
+        """
         self.data = data
-        self.status = 1         # 1 - message correct, 0 - message incorrect
+        self.status = 1
         self.sent = 0
 
         self.type = self.__parse_type(data)
@@ -15,38 +24,102 @@ class Message:
             self.delay = self.__parse_delay(data)
 
     def __eq__(self, other):
+        """
+        Method necessary to compare two message instances
+
+        @param other:   Object to compare
+        @return:        False -> not equal
+                        True  -> equal
+        """
         if isinstance(other, Message):
             return self.data == other.data
         return False
 
     def __hash__(self):
+        """
+        Hash message's data, implemented for __eq__()
+
+        @return:        Hashed message data
+        """
         return hash(self.data)
 
     def get_status(self):
+        """
+        Status getter
+
+        @return:        Message status
+                        0 -> message incorrect
+                        1 -> message correct
+        """
         return self.status
 
     def get_type(self):
+        """
+        Type getter
+
+        @return:        Message type, available:
+                        - delayed
+                        - cancelled
+                        - not recognized
+        """
         return self.type
 
     def get_number(self):
+        """
+        Number getter
+
+        @return:        Train number
+        """
         return self.number
 
     def get_route(self):
+        """
+        Route getter
+
+        @return:        Train route as a dictionary
+                        departure city  :   departure time
+                        arrival city    :   arrival time
+        """
         return self.route
 
     def get_delay(self):
+        """
+        Delay getter
+
+        @return:        Train delay
+        """
         return self.delay
 
     def get_data(self):
+        """
+        Data getter
+
+        @return:        Message content
+        """
         return self.data
 
     def is_sent(self):
+        """
+        Getter for sent status
+
+        @return:        Message sent status
+        """
         return self.sent
 
     def send(self):
+        """
+        Setter for sent status
+
+        """
         self.sent = 1
 
     def __parse_number(self, data):
+        """
+        Train number parser
+
+        @param data:    Message content
+        @return:        Train number
+        """
         temp = re.search(r"Pociąg nr (\d+)", self.data)
 
         if temp:
@@ -56,7 +129,14 @@ class Message:
             self.status = self.status * 0
 
     def __parse_route(self, data):
+        """
+        Route parser
 
+        @param data:    Message content
+        @return:        Train route as a dictionary
+                        departure city  :   departure time
+                        arrival city    :   arrival time
+        """
         x = self.data.find("relacji")  # remove beginning
         if x != -1:
             temp = self.data[x + len("relacji"):]
@@ -73,6 +153,15 @@ class Message:
             self.status = self.status * 0
 
     def __parse_type(self, data):
+        """
+        Type parser
+
+        @param data:    Message content
+        @return:        Message type, available:
+                        - delayed
+                        - cancelled
+                        - not recognized
+        """
         if "opóźniony" in self.data:
             return "delayed"
         elif "odwołany" in self.data:
@@ -82,6 +171,12 @@ class Message:
             return "not recognized"
 
     def __parse_delay(self, data):
+        """
+        Delay parser
+
+        @param data:    Message content
+        @return:        Train delay
+        """
         temp = re.search(r"(\d+)\sminut", self.data)
         if temp:
             return temp.group(1)
