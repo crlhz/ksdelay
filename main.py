1a4208511ccd5565f1ba76360b064933a0faf1d5
crlhz <horzela.kamil@gmail.com>
2024-10-30 13:13:29 +0100

    ref: main docs

--- main.py
+++ main.py
@@ -18,12 +18,15 @@ old_messages = []
 messages = []
 unsent_messages = []
 
+# get raw data from webpage
 scrapper = Scrapper("https://www.kolejeslaskie.com/category/informacje/")
 raw_messages = scrapper.get_data()
 
+# initialize telegram bot
 load_dotenv()
 bot = Bot(os.getenv('BOT_TOKEN'))
 
+# convert raw data into messages
 for mes in raw_messages:
     message = Message(mes)
     if message.get_status() == 1:
@@ -32,18 +35,22 @@ for mes in raw_messages:
     else:
         logging.warning("Not recognized: " + message.get_data())
 
+# take old messages
 try:
     with open('old-messages.pkl', 'rb') as f:
         old_messages = pickle.load(f)
 except (IOError, pickle.PickleError) as e:
     logging.error(f"Wystąpił błąd podczas zapisywania danych: {e}")
 
+# compare new messages with old messages
 unsent_messages = set(messages) - set(old_messages)
 
+# send messages that haven't been sent yet
 for mes in unsent_messages:
     print(mes.get_data())
     bot.send_message(os.getenv("CHANNEL_ID"), mes.get_data())
 print("***Koniec nowych wiadomości***")
 
+# save new messages as old messages
 with open('old-messages.pkl', 'wb') as f:
     pickle.dump(messages, f)
